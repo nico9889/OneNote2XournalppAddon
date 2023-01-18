8a73758124ea1a152bf61d5fc70d424c32ac6a8c
Nicola <nx9889@gmail.com>
2023-01-18 23:09:11 +0100

    Removed unused RegExp

--- src/onenote2xournalpp.ts
+++ src/onenote2xournalpp.ts
@@ -1,5 +1,4 @@
 import browser from "webextension-polyfill";
-const url: RegExp = new RegExp('https?:\/\/onedrive\.live\.com\/.*');
 
 browser.action.onClicked.addListener(async (tab) => {
     await browser.tabs.sendMessage(tab.id ?? 0, {text: 'convert_page'});
